a96063c572b7ac241af7b07127ec642d1a25539d
Dilbar <dilbar.bskdkf20@iba-suk.edu.pk>
2024-04-13 21:13:02 +0500

    Object Detection Using OpenCV

---
+++ Object-Detection-Webcam-OpenCV-master/OBJECT_DETECTION_WEBCAM_GRAY.py
@@ -0,0 +1,83 @@
+"""OBJECT DETECTION WEB CAM"""
+# GRAY SCALE
+# quite similar as object detection
+
+import cv2
+
+# getting vid from cv2.VideoCapture 
+video = cv2.VideoCapture(0)
+a = 1
+path = 'C:\\Users\\kuk\\OBJECT_DETECTION\\OBJECT_DETECTION_WEBCAM\\haar_cascades'
+
+scale_factor = 1
+while True:
+    a = a +1    
+    
+    face_cascade = cv2.CascadeClassifier(path + "\\haarcascade_frontalface_default.xml")
+    eye_cascade = cv2.CascadeClassifier(path +"\\haarcascade_eye.xml")
+    upperbody_cascade = cv2.CascadeClassifier(path + "\\haarcascade_upperbody.xml")
+    smile_cascade = cv2.CascadeClassifier(path+"\\haarcascade_smile.xml")
+    profileface_cascade = cv2.CascadeClassifier(path+"\\haarcascade_profileface.xml")
+    lowerbody_cascade = cv2.CascadeClassifier(path+"\\haarcascade_lowerbody.xml")
+    fullbody_cascade = cv2.CascadeClassifier(path+"\\haarcascade_fullbody.xml")
+    frontalface_alt_cascade = cv2.CascadeClassifier(path+"\\haarcascade_frontalface_alt.xml")
+    # check is a boolian operator that returns TRUE if webcam is working
+    # frame gets the frame(imgs) from the vid camera
+    check, frame = video.read() 
+    gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)    
+    gray_img = cv2.resize(gray_img, (int(gray_img.shape[1]/scale_factor),int(gray_img.shape[0]/scale_factor)))    
+    
+    img=gray_img
+     
+    faces = face_cascade.detectMultiScale(gray_img, 1.05, 5)
+    eyes = eye_cascade.detectMultiScale(gray_img, 1.05, 5)
+    upperbody = upperbody_cascade.detectMultiScale(gray_img, 1.05, 5)
+    smile = smile_cascade.detectMultiScale(gray_img, 1.05, 5)
+    profileface = profileface_cascade.detectMultiScale(gray_img, 1.05, 5)
+    lowerbody = lowerbody_cascade.detectMultiScale(gray_img, 1.05, 5)
+    fullbody = fullbody_cascade.detectMultiScale(gray_img, 1.05, 5)
+    frontalface_alt = frontalface_alt_cascade.detectMultiScale(gray_img, 1.05, 5)
+    
+    for x,y,w,h in faces:
+        rec_img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0) ,0)
+        face_text = cv2.putText(img, "FACE",(x, y+10),cv2.FONT_HERSHEY_COMPLEX_SMALL,.7,(0,255,0))
+        
+    for x,y,w,h in eyes:
+        eye_img = cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 3)
+        eyes_text = cv2.putText(img, "eyes",(x, y+10),cv2.FONT_HERSHEY_COMPLEX_SMALL,.7,(225,0,0))
+        
+    for x,y,w,h in upperbody:
+        upperbody_img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 3)
+        upperbody_text = cv2.putText(img, "upper body",(x, y+10),cv2.FONT_HERSHEY_COMPLEX_SMALL,.7,(0,0,255))
+        
+    for x,y,w,h in smile:
+        smile_img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,100,0), 3)
+        smile_text = cv2.putText(img, "smile",(x, y+10),cv2.FONT_HERSHEY_COMPLEX_SMALL,.7,(0,100,0))
+        
+    for x,y,w,h in profileface:
+        profile_img = cv2.rectangle(img, (x,y), (x+w, y+h), (100,0,0), 3)
+        profile_text = cv2.putText(img, "profile_",(x, y+10),cv2.FONT_HERSHEY_COMPLEX_SMALL,.7,(100,0,0))
+        
+    for x,y,w,h in lowerbody:
+        lowerbody_img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,100), 3)
+        lowerbody_text = cv2.putText(img, "lowerbody",(x, y+10),cv2.FONT_HERSHEY_COMPLEX_SMALL,.7,(0,0,100))
+        
+    for x,y,w,h in fullbody:
+        fullbody_img = cv2.rectangle(img, (x,y), (x+w, y+h), (150,0,0), 3)
+        fullbody_text = cv2.putText(img, "fullbody",(x, y+10),cv2.FONT_HERSHEY_COMPLEX_SMALL,.7,(150,0,0))
+        
+    for x,y,w,h in frontalface_alt:
+        frontalface_img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,150,0), 3)
+        frontalface_text = cv2.putText(img, "frontal_face",(x, y+10),cv2.FONT_HERSHEY_COMPLEX_SMALL,.7,(100,150,0))
+    
+    cv2.imshow('capturing', img)
+    
+    key = cv2.waitKey(1)
+    
+    if key == ord('q'):
+        break
+    
+print(a)
+video.release
+
+cv2.destroyAllWindows()
---
+++ Object-Detection-Webcam-OpenCV-master/README.md
@@ -0,0 +1,21 @@
+# Object Detection in Videos using OpenCV
+
+## About
+This is 3 of 3 notebook that shows the use of OpenCV to detect objects in video from webcams. 
+Fist notebook 1/3 shows the implementation of OpenCV to detect Object in images which can be found [here](https://github.com/deep28vish/Object-Detection-Images-OpenCV). [Second Notebook](https://github.com/deep28vish/Object-detection-Videos-OpenCV) is Object detectio on Videos.
+
+These are sample codes ready to run without any prior knowledge of AI, ML DNN, CNN these 50 lines are just plain simple ready to run.
+
+## Requirements
+* [Opencv(cv2)](https://pypi.org/project/opencv-python/)
+* [Cascade classifiers](https://github.com/deep28vish/Object-Detection-Images-OpenCV/tree/master/haar_cascades)
+## Contents
+* haar_cascades[Folder with all the required classifiers]
+* OBJECT_DETECTION_WEBCAM_COLORISED.py
+* OBJECT_DETECTION_WEBCAM_GRAY.py
+    
+## Inspiration
+* [SENTDEX](https://pythonprogramming.net/)
+
+## Takeaway
+This is just to see how to load classifiers and execute it using OPENCV in python for webcam Videos.
